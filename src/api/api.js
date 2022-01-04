8eb4ecef6949cdb4ea2e3bfdf44c1d10b850a4d3
Ruslan Garaev <r.garaevs@gmail.com>
2022-01-04 17:39:20 +0300

    изменил api-key

--- src/api/api.js
+++ src/api/api.js
@@ -1,59 +1,59 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        'API-KEY': '2a3910d4-c3b0-448e-a4c3-d58254f5e107',
-    },
+	withCredentials: true,
+	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+	headers: {
+		'API-KEY': 'c899a927-34b5-4c55-b016-a5d2dd34b15d',
+	},
 });
 
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
-            return response.data;
-        });
-    },
-    follow(userId) {
-        return instance.post(`follow/${userId}`);
-    },
-    unfollow(userId) {
-        return instance.delete(`follow/${userId}`);
-    },
-    getProfile(userId) {
-        return profileAPI.getProfile(userId);
-    },
+	getUsers(currentPage = 1, pageSize = 10) {
+		return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+			return response.data;
+		});
+	},
+	follow(userId) {
+		return instance.post(`follow/${userId}`);
+	},
+	unfollow(userId) {
+		return instance.delete(`follow/${userId}`);
+	},
+	getProfile(userId) {
+		return profileAPI.getProfile(userId);
+	},
 };
 
 export const profileAPI = {
-    getProfile(userId) {
-        return instance.get('profile/' + userId);
-    },
-    getStatus(userId) {
-        return instance.get('profile/status/' + userId);
-    },
-    updateStatus(status) {
-        return instance.put('profile/status/', { status: status });
-    },
-    savePhoto(photoFile) {
-        const formData = new FormData();
-        formData.append('image', photoFile);
-        return instance.put('profile/photo/', formData, {
-            headers: {
-                'Content-type': 'multipart/form-data',
-            },
-        });
-    },
+	getProfile(userId) {
+		return instance.get('profile/' + userId);
+	},
+	getStatus(userId) {
+		return instance.get('profile/status/' + userId);
+	},
+	updateStatus(status) {
+		return instance.put('profile/status/', { status: status });
+	},
+	savePhoto(photoFile) {
+		const formData = new FormData();
+		formData.append('image', photoFile);
+		return instance.put('profile/photo/', formData, {
+			headers: {
+				'Content-type': 'multipart/form-data',
+			},
+		});
+	},
 };
 
 export const authAPI = {
-    me() {
-        return instance.get('auth/me');
-    },
-    login(email, password, rememberMe = false) {
-        return instance.post('auth/login', { email, password, rememberMe });
-    },
-    logout() {
-        return instance.delete('auth/login');
-    },
+	me() {
+		return instance.get('auth/me');
+	},
+	login(email, password, rememberMe = false) {
+		return instance.post('auth/login', { email, password, rememberMe });
+	},
+	logout() {
+		return instance.delete('auth/login');
+	},
 };
